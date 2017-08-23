54b853c94843521e0d7e911167b326ada853909b
Terence Buchannan <terencebuchannan@gmail.com>
2017-08-22 23:17:18 -0400

    Add solution for two_sum

--- two_sum.js
+++ two_sum.js
@@ -9,3 +9,13 @@
 //
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length -1; i++){
+        for (let j = i + 1; j <nums.length; j++){
+            if (nums[i] + nums[j] === target){
+                return [i, j];
+            }
+        }
+    }
+};

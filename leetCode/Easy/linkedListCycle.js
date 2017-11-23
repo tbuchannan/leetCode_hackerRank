a74cfa13a443417c55f17386f4977824f2a96e99
Terence Buchannan <terencebuchannan@gmail.com>
2017-11-23 12:40:00 -0500

    Linked List Cycle: Add Solution

---
+++ leetCode/Easy/linkedListCycle.js
@@ -0,0 +1,32 @@
+/*
+ * 141. Linked List Cycle
+ *
+ *  Given a linked list, determine if it has a cycle in it.
+ *
+ *  Follow up:
+ *  Can you solve it without using extra space?
+*/
+
+
+const hasCycle = (head) => {
+    if (head === null || head.next === null){
+        return false;
+    }
+    let slowPointer = head;
+    let fastPointer = head.next;
+
+    while (slowPointer.next !== null){
+      if (slowPointer === fastPointer) {
+        return true;
+      }
+      if(fastPointer === null || fastPointer.next === null){
+          return false;
+      }else {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+
+    }
+
+};

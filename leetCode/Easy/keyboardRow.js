a890e184088206871d07775513a28731a168cd29
Terence Buchannan <terencebuchannan@gmail.com>
2017-12-15 17:41:12 -0500

    KeyboardRow: Add Solution

--- leetCode/Easy/keyboardRow.js
+++ leetCode/Easy/keyboardRow.js
@@ -12,3 +12,37 @@
 // Note:
 // You may use one character in the keyboard more than once.
 // You may assume the input string will only contain letters of alphabet.
+
+// Can be solved with some stupid regex one liner.....
+
+const findWords = (words) => {
+  let result = [];
+  let rows = {
+    "q":1, "w":1, "e":1, "r":1, "t":1, "y":1, "u":1, "i":1, "o":1, "p":1,
+    "a":2, "s":2, "d":2, "f":2, "g":2, "h":2, "j":2, "k":2, "l":2,
+    "z":3, "x":3, "c":3, "v":3, "b":3, "n":3, "m":3
+  };
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let row = rows[word[0].toLowerCase()];
+
+    if (singleRow(rows, word, row)) {
+      result.push(word);
+    }
+  }
+
+  return result;
+};
+
+const singleRow = (rows, word, row) => {
+
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i].toLowerCase();
+    if (rows[letter] !== row ) {
+      return false;
+    }
+  }
+
+  return true;
+};

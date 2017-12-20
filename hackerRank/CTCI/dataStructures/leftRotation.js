70fad5a6ef76c61fb69daf0d86569066ffc516ce
Terence Buchannan <terencebuchannan@gmail.com>
2017-12-20 13:52:27 -0500

    Left Rotation: Add Solution

---
+++ hackerRank/CTCI/dataStructures/leftRotation.js
@@ -0,0 +1,30 @@
+// Arrays: Left Rotation
+
+// Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+//
+// Input Format
+//
+// The first line contains two space-separated integers denoting the respective values of  (the number of integers) and  (the number of left rotations you must perform).
+// The second line contains  space-separated integers describing the respective elements of the array's initial state.
+//
+// Constraints
+//
+// Output Format
+//
+// Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+//
+// Input
+// 5 4
+// 1 2 3 4 5
+
+// Sample Output
+// 5 1 2 3 4
+// Explanation
+//
+// When we perform  left rotations, the array undergoes the following sequence of changes:
+//
+// Thus, we print the array's final state as a single line of space-separated values, which is 5 1 2 3 4.
+
+const leftRotation = (rotations, arr) => {
+  return (arr.slice(rotations).concat(arr)).slice(0, arr.length);
+};
